9fc685229c82048272f94a89ff6338bbb762bc9b
12934713 <12934713@e1-b1400-15.ad.feit.uts.edu.au>
2017-06-02 21:54:13 +1000

    finished the main pizzeria page

--- src/controller/PizzeriaController.java
+++ src/controller/PizzeriaController.java
@@ -1,10 +1,13 @@
 package controller;
 
 import au.edu.uts.ap.javafx.Controller;
+import au.edu.uts.ap.javafx.ViewLoader;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 import model.Customer;
 import model.Pizzeria;
 
@@ -18,6 +21,18 @@ public class PizzeriaController extends Controller<Pizzeria> {
     @FXML public void initialize() {
         Image img = new Image("file:pizzeria.png");
         ImageView imageView = new ImageView(img);
+    }
+    
+    @FXML protected void handleAddCustomerAction(ActionEvent event) throws Exception {
+        ViewLoader.showStage(null, "/view/customer_add.fxml", "Add customer", new Stage());
+    }
+    
+    @FXML protected void handleServeCustomerAction(ActionEvent event) throws Exception {
+        ViewLoader.showStage(getSelectedCustomer(), "/view/serve.fxml", "Serve customer", new Stage());
+    }
+    
+    @FXML protected void handleViewReportAction(ActionEvent event) throws Exception {
+        ViewLoader.showStage(null, "/view/report.fxml", "Income report", new Stage());
     }
 
     public final Pizzeria getPizzeria() {
--- src/controller/ServeController.java
+++ src/controller/ServeController.java
@@ -1,4 +1,15 @@
 package controller;
 
+import au.edu.uts.ap.javafx.Controller;
+import au.edu.uts.ap.javafx.ViewLoader;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import model.Customer;
+import model.Pizzeria;
+
 public class ServeController {
 }
