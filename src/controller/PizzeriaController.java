e4b509c945bc4bf4b685e81923f06a0c33873d75
12934713 <12934713@e1-b1400-15.ad.feit.uts.edu.au>
2017-06-02 17:52:26 +1000

    finished main page

--- src/controller/PizzeriaController.java
+++ src/controller/PizzeriaController.java
@@ -2,12 +2,19 @@ package controller;
 
 import au.edu.uts.ap.javafx.Controller;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import model.Customer;
 import model.Pizzeria;
 
 public class PizzeriaController extends Controller<Pizzeria> {
-
+    @FXML private ListView<Customer> customersLv;
+    
+    private Customer getSelectedCustomer() {
+        return customersLv.getSelectionModel().getSelectedItem();
+    }
+    
     @FXML public void initialize() {
         Image img = new Image("file:pizzeria.png");
         ImageView imageView = new ImageView(img);
