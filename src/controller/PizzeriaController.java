dd1fc30f6054edd30d339063cf3eca475229c631
12934713 <12934713@e1-b1400-15.ad.feit.uts.edu.au>
2017-06-02 16:56:17 +1000

    the pizzeria content is partially done

--- src/controller/PizzeriaController.java
+++ src/controller/PizzeriaController.java
@@ -2,11 +2,15 @@ package controller;
 
 import au.edu.uts.ap.javafx.Controller;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import model.Pizzeria;
 
 public class PizzeriaController extends Controller<Pizzeria> {
 
     @FXML public void initialize() {
+        Image img = new Image("file:pizzeria.png");
+        ImageView imageView = new ImageView(img);
     }
 
     public final Pizzeria getPizzeria() {
